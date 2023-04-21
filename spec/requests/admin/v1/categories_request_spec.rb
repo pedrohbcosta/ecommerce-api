95b8e41baa76686be45dd30e386c89730f7bb657
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-04-21 15:14:45 -0300

    typo

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -116,7 +116,7 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     end
   end
 
-  context "DELETE /categories" do
+  context "DELETE /categories/:id" do
     let!(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" }
 
